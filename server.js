fc4efb7cca8ffe3e63126496066002640492a245
cristinefolz <cristinefolz@gmail.com>
2016-02-16 12:01:38 -0700

    create database, create BearSchema in bear.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+// when starting up, use the 'animals' database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/animals'); //animals = the database that exists or will be created
+
 // mounting/requiring/using middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,10 +17,10 @@ var port = process.env.PORT || 8080;   // set our port
 var router = express.Router();   // get an instance of router
 
 router.get('/', function(req, res){
-	res.json({message: 'horray! welcome to our api!'});
+	res.json({message: "horray! you are looking at Cristine's CRUD bear project!"});
 });
 
-
+// all of our routes will be prefixed with /api
 app.use('/api', router);
 
 
