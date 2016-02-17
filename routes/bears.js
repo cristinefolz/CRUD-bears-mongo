bbc3eedcaedffbb9806444b41e27712a3c62543a
cristinefolz <cristinefolz@gmail.com>
2016-02-17 11:24:12 -0700

    refactor: create router directory and add bears.js to hold CRUD functions

---
+++ routes/bears.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();  // get an instance of router
+var Bear = require('../models/bear');
+
+router.route('/bears')
+	.post(function(req, res){
+
+		var bear = new Bear();
+
+		bear.name = req.body.name;
+		bear.age = req.body.age;
+		bear.gender = req.body.gender;
+
+		bear.save(function(err, bear){
+			if(err){
+				console.log(err);
+			} else {
+				res.json(bear);
+			}
+		})
+	})
+
+    .get(function(req, res){
+    	Bear.find(function(err, bears){   //node style call back (err, bears).  If all goes well with Bear.find, the results are returned in the 'bear' argument.  If something goes wrong, the first 'err' argument will be populated with an error object containing information about the problem.
+    		if(err){
+    			console.log(err);
+    		} else {
+    			res.json(bears);
+    		}
+    	})
+    });
+
+
+
+router.route('/bears/:bear_id')
+   .get(function(req, res){
+   	 Bear.findById(req.params.bear_id, function(err, bear){  //req.params because the bear_id is coming in from the URL; findById comes with Mongoose
+ 		if(err){
+ 			console.log(err);
+ 		} else {
+ 			res.json(bear);
+ 		}
+   	 })
+   })
+
+   .put(function(req, res){
+   	 Bear.findById(req.params.bear_id, function(err, bear){
+   	 	if(err){
+   	 		console.log(err);
+   	 	} else {
+
+   	 		bear.name = req.body.name ? req.body.name : bear.name;  //ternary expression (always returns a value)
+   	 		bear.age = req.body.age ? req.body.age : bear.age;
+   	 		bear.gender = req.body.gender ? req.body.gender : bear.gender;
+
+   	 		bear.save(function(err, newBear){
+   	 			if(err){
+   	 				console.log(err);
+   	 			} else {
+   	 				res.json({title: 'bear updated'});
+   	 			}
+   	 		})
+   	 	}
+   	 })
+   	})
+
+   .delete(function(req, res){
+   	 Bear.remove({_id: req.params.bear_id}, function(err, bear){
+   	 	if(err){
+ 			console.log(err);
+   	 	} else{
+   	 		res.json({title: 'bear was successfully deleted'});
+   	 	}
+   	 })
+   });
+
+ //  .post();
+
+ module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/animals'); //animals = the database that exists or will be created
 
+var bearRouter = require('./routes/bears');
+
 var Bear = require('./models/bear');
 
 // mounting/requiring/using middleware
@@ -24,92 +26,13 @@ router.use(function(req, res, next){
 });
 
 
-
-
 router.get('/', function(req, res){
 	res.json({message: "horray! you are looking at Cristine's CRUD bear project!"});
 });
 
-router.route('/bears')
-	.post(function(req, res){
-
-		var bear = new Bear();
-
-		bear.name = req.body.name;
-		bear.age = req.body.age;
-		bear.gender = req.body.gender;
-
-		bear.save(function(err, bear){
-			if(err){
-				console.log(err);
-			} else {
-				res.json(bear);
-			}
-		})
-	})
-
-    .get(function(req, res){
-    	Bear.find(function(err, bears){   //node style call back (err, bears)
-    		if(err){
-    			console.log(err);
-    		} else {
-    			res.json(bears);
-    		}
-    	})
-    });
-
-
-
-router.route('/bears/:bear_id')
-   .get(function(req, res){
-   	 Bear.findById(req.params.bear_id, function(err, bear){  //req.params because the bear_id is coming in from the URL; findById comes with Mongoose
- 		if(err){
- 			console.log(err);
- 		} else {
- 			res.json(bear);
- 		}
-   	 })
-   })
-
-   .put(function(req, res){
-   	 Bear.findById(req.params.bear_id, function(err, bear){
-   	 	if(err){
-   	 		console.log(err);
-   	 	} else {
-
-   	 		bear.name = req.body.name ? req.body.name : bear.name;  //ternary expression (always returns a value)
-   	 		bear.age = req.body.age ? req.body.age : bear.age;
-   	 		bear.gender = req.body.gender ? req.body.gender : bear.gender;
-
-   	 		bear.save(function(err, newBear){
-   	 			if(err){
-   	 				console.log(err);
-   	 			} else {
-   	 				res.json({title: 'bear updated'});
-   	 			}
-   	 		})
-   	 	}
-   	 })
-   	})
-
-   .delete(function(req, res){
-   	 Bear.remove({_id: req.params.bear_id}, function(err, bear){
-   	 	if(err){
- 			console.log(err);
-   	 	} else{
-   	 		res.json({title: 'bear was successfully deleted'});
-   	 	}
-   	 })
-   });
-
- //  .post();
-
-
-
-
 
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', bearRouter);
 
 app.listen(port);
 console.log('magic happens on port '+ port);
